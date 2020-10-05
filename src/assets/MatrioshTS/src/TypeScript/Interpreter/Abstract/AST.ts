72689d0fcaf2e6ad9e33a5d863888a06a96663dd
racarlosdavid <2111724640101@ingenieria.usac.edu.gt>
2020-10-05 00:57:18 -0600

    update

--- src/assets/MatrioshTS/src/TypeScript/Interpreter/Abstract/AST.ts
+++ src/assets/MatrioshTS/src/TypeScript/Interpreter/Abstract/AST.ts
@@ -27,7 +27,7 @@ export class AST {
     traducir(builder: StringBuilder, parent:string):any { console.log("Ok. Vamos a traducir la cadena de entrada");
         //Primera pasada para obtener los nuevos ids los almaceno en la variable traductor de Manager.ts
         let builder_inservible = new StringBuilder(); 
-        /*Para esta pasade le mando un stringbuilder solo para cumplir con el parametro y que no me genere doble salida 
+        /*Para esta pasada le mando un stringbuilder solo para cumplir con el parametro y que no me genere doble salida 
         ya que en la siguiente pasada le mando el builder que va a recolectar el codigo traducido */
 
         for(const instr of this.instrucciones){
@@ -48,20 +48,10 @@ export class AST {
                 console.log("Error en la traduccion: "+error);
             }
         }
-
        return builder.toString();
     }
     
     public ejecutar(ent:Entorno, er:ErrorManager, consola:StringBuilder, tsCollector:TSCollector, reporte_ts:R_TS, ambito:string, padre:string):any { console.log("Ok. Vamos a interpretar la cadena de entrada");
-        /*
-        for(const instr of this.instrucciones){
-            try {
-                instr.ejecutar(ent,er,consola,tsCollector);
-            } catch (error) {
-                console.log("Error en la interpretacion: "+error);
-            }
-        }
-        */
         //Primera pasada guarda los types en la tabla de simbolos
         for (const ins of this.instrucciones) {
             try {
@@ -72,6 +62,7 @@ export class AST {
                 console.log("Error en la interpretacion: Primera Pasada"+error);
             }
         }
+
         //Segunda pasada guarda las funciones en la tabla de simbolos
         for (const ins of this.instrucciones) {
             try {
@@ -83,7 +74,6 @@ export class AST {
             }
         }
         
-
         //Tercera pasada ejecuto todo lo demas
         for (const ins of this.instrucciones) {
             try {
